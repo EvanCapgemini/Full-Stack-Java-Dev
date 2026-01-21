d3f1e457ce529098cfa568f5d316cc33a294c05d
EvanCapgemini <evan.elijah-mendonsa@capgemini.com>
2026-01-21 08:46:59 +0530

    Add files via upload

---
+++ Java/Getting Started with Java/L3_strings.java
@@ -0,0 +1,18 @@
+public class L3_strings {
+    public static void main(String[] args) {
+        String var1 = "Evan";
+        String var2 = " Mendonsa";
+        System.out.println(var1+var2);
+        //Adding numbers if they were a String:-
+        String num1 = "7";
+        int num2 = 9;
+        System.out.println(Integer.parseInt(num1)+num2);
+    }
+}
+
+/*
+Notes:-
+* char data-type makes use of '...'
+* String data-type makes use of "..." --> It is a class, hence Capital. Also, it is non-primitive.
+* '+' --> means concatenation.
+*/
---
+++ Java/Getting Started with Java/L4_type_casting/Part2.java
@@ -0,0 +1,17 @@
+package L4_type_casting;
+
+public class Part2 {
+    public static void main(String[] args) {
+        //Parsing:
+        String num1 = "11";
+        int temp = Integer.parseInt(num1);
+        System.out.println(temp);
+    }
+}
+
+/*
+Notes:-
+
+* parsing --> Reading the text and extracting information from it.
+* In type casting, range (not memory size required) matters a-lot when determining whether it can be Implicitly or Explicitly type-casted.
+*/
